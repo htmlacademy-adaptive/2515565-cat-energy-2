01eb6a8c1dd3d6d72081e0ef2b8d129961ddba1d
gostyukhin <154966287+gostyukhin@users.noreply.github.com>
2025-01-03 19:29:49 +0700

    Добавляет векторные изображения (#6)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,6 @@
 /* в этот файл добавляет скрипты*/
+// BURGER
+const burgerButton = document.querySelector('.navigation__toggle');
+burgerButton.addEventListener('click', () => {
+  burgerButton.classList.toggle('navigation__toggle--active');
+});
