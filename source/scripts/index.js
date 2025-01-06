41dbeda007c85e34b294d3e5e6660ed143b4b30e
gostyukhin <154966287+gostyukhin@users.noreply.github.com>
2025-01-06 21:50:43 +0700

    Добавляет адаптивные сетки крупных блоков (#7)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,18 @@
 /* в этот файл добавляет скрипты*/
 // BURGER
-const burgerButton = document.querySelector('.navigation__toggle');
+const burgerButton = document.querySelector('.button-toggle');
+const burgerMenu = document.querySelector('.navigation__list');
 burgerButton.addEventListener('click', () => {
-  burgerButton.classList.toggle('navigation__toggle--active');
+  burgerButton.classList.toggle('button-toggle--active');
+  burgerMenu.classList.toggle('open');
+  document.body.style = 'overflow-y: hidden';
+
+  if (!burgerMenu.classList.contains('open')) {
+    document.body.style = 'overflow-y: visible';
+  }
+});
+
+const cardButtonShow = document.querySelector('.card__button-show');
+cardButtonShow.addEventListener('click', () => {
+  cardButtonShow.classList.toggle('card__button-show--active');
 });
